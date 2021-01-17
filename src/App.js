dcc6f2de33515821c08eb892fc5002bec7cfd91b
Pratik <pratikroychowdhury91011@gmail.com>
2021-01-17 21:07:08 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+      <Switch>
+        <Route exact path="/">
+          This is HOME
+        </Route>
+        
+        <Route exact path="/starred">
+          This is STARRED
+        </Route>
+
+        
+        <Route>
+          <h1>404
+          </h1>
+        </Route>
+      </Switch>
   );
 }
 
